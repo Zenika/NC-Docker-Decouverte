d9c77c193d36ee57765a6631b2a3e154ab636c2b
Vincent Demeester <vincent+github@demeester.fr>
2015-02-05 17:18:53 +0000

    Update setup_debian.sh

--- Hands-on/setup_debian.sh
+++ Hands-on/setup_debian.sh
@@ -1,5 +1,7 @@
-# Prérequis:
+#!/bin/bash
 sudo apt-get update
+sudo apt-get install -y git
+git clone https://github.com/Zenika/NC-Docker-Decouverte.git
 sudo apt-get install -y htop qemu curl vim
 sudo apt-get install -y qemu-kvm qemu-utils
 curl -sSL https://get.docker.com/ | sh
